d6b8f17e41b4adafa468958a6d9807a118ea10b9
Hendrik <hendrik.saly@eliatra.com>
2024-03-06 11:23:19 +0100

    Corrected EOD log output (#26)

--- server/src/main/java/org/opensearch/node/Node.java
+++ server/src/main/java/org/opensearch/node/Node.java
@@ -346,7 +346,7 @@ public class Node implements Closeable {
                 .put(NODE_ATTRIBUTES.getKey() + SHARD_INDEXING_PRESSURE_ENABLED_ATTRIBUTE_KEY, "true")
                 .build();
 
-            logger.info("Eliatra OpenSearch Enterprise Distro (https://eliatra.com/opensearch-enterprise-distro)");
+            logger.info("Eliatra OpenSearch Distro (https://eliatra.com/opensearch-distro)");
 
             final JvmInfo jvmInfo = JvmInfo.jvmInfo();
             logger.info(
